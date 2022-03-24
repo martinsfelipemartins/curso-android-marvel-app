a6b58ea5aa4a0e44c317ef8b858aedc4cd61f5dc
martinsfelipemartins <martins.felipe1@hotmail.com>
2022-03-23 21:12:29 -0300

    configura container de navegacao do projeto

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.
+        findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController =  navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(setOf())
     }
 }
