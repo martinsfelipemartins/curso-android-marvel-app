40d8c93fcd82b619a051d8744b9aa7a300856fb8
martinsfelipemartins <martins.felipe1@hotmail.com>
2022-07-22 23:13:45 -0300

    arruma clean arq projeto

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,17 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.core.domain.model.Character
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentCharacteresBinding
 
 class CharactersFragment : Fragment() {
 
+    lateinit var binding: FragmentCharacteresBinding
+    private val  adapterChar= CharactersAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_characteres, container, false)
+    ): View = FragmentCharacteresBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+        adapterChar.submitList(
+            listOf(
+                Character("Spider",
+                    "https://images.freeimages.com/images/large-previews/b23/d-link-switch-1243624.jpg"),
+                Character("Spider",
+                    "https://images.freeimages.com/images/large-previews/b23/d-link-switch-1243624.jpg"),
+                Character("Spider",
+                    "https://images.freeimages.com/images/large-previews/b23/d-link-switch-1243624.jpg"),
+                Character("Spider",
+                    "https://images.freeimages.com/images/large-previews/b23/d-link-switch-1243624.jpg"),
+                Character("Spider",
+                    "https://images.freeimages.com/images/large-previews/b23/d-link-switch-1243624.jpg")
+        ))
     }
 
+    private fun initCharactersAdapter() {
+        with(binding.recyclerCharacters){
+            setHasFixedSize(true)
+            adapter =  adapterChar
+        }
+    }
 }
---
+++ core/src/main/java/com/example/core/domain/model/Character.kt
@@ -0,0 +1,6 @@
+package com.example.core.domain.model
+
+data class Character(
+    val name: String,
+    val imageURL: String
+)
